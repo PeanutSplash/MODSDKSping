35a19d7995b7dd00c0d4af27d8afd4f55f2d334e
PeanutSplash <b1300658700@outlook.com>
2025-09-12 23:55:59 +0800

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mcmod-cli',
-    version='1.0.8',
+    version='1.1.0',
     description = "A CLI tool for generating Minecraft Mod project structure and managing MODSDK development.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
